cd5250ee605e5186bc9ebc62a9ad11d45c249938
MoraXX-D <shubham.21dec03@gmail.com>
2023-04-04 21:21:00 +0530

    Day 20

---
+++ consoverloading.java
@@ -0,0 +1,25 @@
+class student 
+{
+    student()
+    {
+        System.out.println("Default cons");
+    }
+    student(int a)
+    {
+        System.out.println(" a ="+a);
+    }
+    student(float a)
+    {
+        System.out.println(" a ="+a);
+    }
+    student(float a,float b)
+    {
+        System.out.println(" a ="+a +"b ="+b);
+    }
+}
+public class consoverloading {
+    public static void main(String[] args) {
+        
+    }
+    
+}
