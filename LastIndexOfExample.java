9bbca78573bbdc4e42c8e1c7e509bcfc81f16bc6
MoraXX-D <shubham.21dec03@gmail.com>
2023-04-07 23:56:05 +0530

    Day 23

---
+++ LastIndexOfExample.java
@@ -0,0 +1,11 @@
+public class LastIndexOfExample{
+    public static void main(String args[]){
+    String s1="hello";
+    String s2="hello";
+    String s3="meklo";
+    String s4="hemlo";
+    System.out.println(s1.compareTo(s2));
+    System.out.println(s1.compareTo(s3));
+    System.out.println(s1.compareTo(s4));
+    }}
+    
