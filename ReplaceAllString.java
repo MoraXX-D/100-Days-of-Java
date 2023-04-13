50c2c0964b3341d1b0d95ecb030c683f962abf60
MoraXX-D <shubham.21dec03@gmail.com>
2023-04-13 23:56:16 +0530

    Day 24

---
+++ ReplaceAllString.java
@@ -0,0 +1,6 @@
+public class ReplaceAllString{
+public static void main(String args[]){
+String s1="My name is Rajendra. My name is lamror. My name is Technolamror.";
+String replaceString=s1.replaceAll("is","was");//replaces all occurrences of "is" to was
+System.out.println(replaceString);
+}}
