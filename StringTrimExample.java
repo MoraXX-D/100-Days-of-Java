c3ff50a13f0e2384a62857b8019519878dfd78df
MoraXX-D <shubham.21dec03@gmail.com>
2023-04-15 23:15:12 +0530

    Day 25

---
+++ StringTrimExample.java
@@ -0,0 +1,7 @@
+public class StringTrimExample{
+    public static void main(String args[]){
+    String s1=" hello string ";
+    System.out.println(s1+"Shubham");//without trim()
+    System.out.println(s1.trim()+"Shubham");//with trim()
+    }}
+    
