5cf17b617eba4fea600727169d132e80cabb7d26
MoraXX-D <shubham.21dec03@gmail.com>
2023-04-17 22:39:58 +0530

    Day 27

---
+++ toLowerCase.java
@@ -0,0 +1,8 @@
+public class toLowerCase{
+    public static void main(String args[]){
+    String s1="TECHNOLAMROR by Rajendralamror HELLO stRIng";
+    String s1lower=s1.toLowerCase();
+    System.out.println(s1lower);
+}}
+
+    
